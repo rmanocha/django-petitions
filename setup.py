febca982db44890accf4fcb1d45ace93a1907c74
Rishabh Manocha <rmanocha@gmail.com>
2010-03-13 14:15:04 +0800

    Adding a setup.py file so that this code can be installed via pip.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="django-petitions",
+    version="0.1",
+    description="A simple django app to add Petition functionality into your project",
+    author="Rishabh Manocha",
+    author_email="rmanocha@gmail.com",
+    url="http://github.com/rmanocha/django-petitions",
+    packages=["petitions"],
+    zip_safe = False,
+)
